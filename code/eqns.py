c15ae99b41f3c6c96da36ebd2989c70311509aca
James Davenport <jradavenport@gmail.com>
2021-04-13 10:35:54 -0700

    trying lots of things

--- code/eqns.py
+++ code/eqns.py
@@ -11,6 +11,7 @@ def MM09e2(B_V, age):
     P = np.sqrt(age) * (np.sqrt(B_V - a)) - b * (B_V - a)
     return P
 
+
 def MM09e3(B_V, age):
     ''' 
     Eqn 3 
@@ -21,7 +22,6 @@ def MM09e3(B_V, age):
     f = 0.60
     P = age**0.52 * (c * (B_V - d)**f)
     return P
-
 
 
 def Angus2015(B_V, age):
@@ -44,7 +44,6 @@ def Angus2015_age(B_V, P):
     '''
     invert the above eqn
     '''
-    # P = (age ** 0.55) * 0.4 * ((B_V - 0.45) ** 0.31)
     age = np.power(P / (0.4 * ((B_V - 0.45) ** 0.31)), 1. / 0.55)
     return age
 
@@ -106,7 +105,7 @@ def _gaus(x, a, b, x0, sigma):
     return a * np.exp(-(x - x0)**2 / (2 * sigma**2)) + b
 
 
-def CranmerSaar2010_eqn36(Teff):
+def CranmerSaar2011_eqn36(Teff):
     """
     https://ui.adsabs.harvard.edu/abs/2011ApJ...741...54C
     
@@ -118,3 +117,33 @@ def CranmerSaar2010_eqn36(Teff):
     return tau
 
 
+def teff2bv(teff, logg=4.3, feh=0):
+    """
+    Relation from Sekiguchi & Fukugita (2000)
+    https://arxiv.org/abs/astro-ph/9904299
+    """    
+    # from their Tbl 4:
+    t = [-813.3175, 684.4585, -189.923, 17.40875]
+    f = [1.2136, 0.0209]
+    d1, g1, e1 = -0.294, -1.166, 0.3125
+    BV = t[0] + t[1]*np.log10(teff) + t[2]*(np.log10(teff))**2 + \
+            t[3]*(np.log10(teff))**3 + f[0]*feh + f[1]*feh**2 \
+            + d1*feh*np.log10(teff) + g1*logg + e1*logg*np.log10(teff)
+    return BV
+
+
+def bv2teff(BV, logg=4.3, feh=0):
+    """
+    Relation from Sekiguchi & Fukugita (2000)
+    https://arxiv.org/abs/astro-ph/9904299
+    """
+    # Full Sample, Tbl 3
+    c = np.array([3.939654, -0.395361, 0.2082113, -0.0604097])
+    f1, f2, g1, h1 = 0.027153, 0.005036, 0.007367, -0.01069
+    
+    logTeff = c[0] + c[1]*BV + c[2]*(BV**2) + c[3]*(BV**3) + \
+               f1*feh + f2*(feh**2) + \
+               g1*logg + h1*BV*logg
+    return 10**logTeff
+    
+ 
